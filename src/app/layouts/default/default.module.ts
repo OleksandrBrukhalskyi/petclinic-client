15db598fa8aacd600e14dac2e9cc694cb785bd10
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-18 19:49:20 +0300

    feat: create default module

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { OwnersComponent } from 'src/app/modules/owners/owners.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import {MatDividerModule, MatSidenavModule} from '@angular/material';
 
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    OwnersComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule
+    
   ]
 })
 export class DefaultModule { }

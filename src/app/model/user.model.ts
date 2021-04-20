a47132fb38cf8be4db06ec5cb7cf294305a68a4b
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-20 21:26:46 +0300

    feat: create user model.

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,24 @@
+﻿export class User {
+    id: number;
+    surname: string;
+    firstname: string;
+    patronymic: string;
+    email: string;
+    login: string;
+    password: string;
+    role: string;
+    token?: string;
+
+    constructor(surname: string, firstname: string, patronymic: string,
+                email: string, login: string, password: string) {
+            this.surname = surname;        
+            this.firstname = firstname;
+            this.patronymic = patronymic;
+            this.email = email;
+            this.login = login;
+            this.password = password;
+            
+
+    }
+
+}

ab0f2d88938a36539dcefee02894e066a64c5b93
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-22 21:31:41 +0300

    feat: create pet's typescript component.

--- src/app/modules/pets/pets.component.ts
+++ src/app/modules/pets/pets.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
+import { Pet } from 'src/app/model/pet.model';
+import { OwnerService } from 'src/app/services/owner.service';
+import { PetService } from 'src/app/services/pet.service';
 
 @Component({
   selector: 'app-pets',
@@ -6,10 +11,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pets.component.scss']
 })
 export class PetsComponent implements OnInit {
+  displayedColumns = ['id', 'name', 'breed', 'dateOfBirth', 'owner', 'actions'];
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  
+  private owners: any;
+  private pets: any;
+  public dataSource: any;
+  private pet: Pet;
 
-  constructor() { }
 
-  ngOnInit() {
+  petForm: FormGroup;
+
+
+
+  constructor(private ownerService: OwnerService, private petService: PetService, private formBuilder: FormBuilder) {
+      this.petForm = this.formBuilder.group([{
+          name: ['', [Validators.required]],
+          breed: ['',[Validators.required]],
+          dateOfBirth: ['',[Validators.required]],
+          owner: ['',[Validators.required]]
+      }]);
+
+
   }
+  public get f() {
+    return this.petForm.controls;
+  }
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource();
+    this.getPets();
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.getOwners();
+
+
+  }
+  load() {
+  	this.petService.getPets().subscribe((data: {}) => {
+  	  this.dataSource.data = data;
+  	  this.pets = data;
+  	});
+  }
+  create() {
+    this.petService.add(this.pet).subscribe(data => {
+      this.load();
+      this.petForm.setErrors(null);
+      this.petForm.reset();
+
+    });
+
+  }
+  update() {
+
+  }
+  getPets() {
+    this.petService.getPets().subscribe((data: {}) => {
+      this.dataSource.data = data;
+      this.pets = data;
+    });
+
+  }
+  
+  getOwners() {
+    this.petService.getPets().subscribe(data => {
+      this.owners = data;
+    });
+
+  }
+  delete(id: any) {
+    this.petService.delete(id).subscribe(() => {
+      this.getPets();
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
 }

2961d49362ac1e36697eaaf43ee085bdd659a0a4
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-22 18:18:07 +0300

    fix: refactor login component

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -23,10 +23,7 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private authenticationService: AuthService) {
       
-// if (this.authenticationService.userValue) { 
-// this.router.navigate(['/']);
-// return;
-// }
+
 
 }
 

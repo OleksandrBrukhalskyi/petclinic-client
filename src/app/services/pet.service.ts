e6654386cab9f4006738c8fb0adfa7b3eb0b3e3e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-22 21:28:25 +0300

    feat: create pet service.

---
+++ src/app/services/pet.service.ts
@@ -0,0 +1,27 @@
+﻿import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Pet } from "../model/pet.model";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class PetService {
+    baseUrl = `http://localhost:8080/api/pet`;
+    constructor(private http: HttpClient) { }
+  
+    getPets() {
+      return this.http.get(`${this.baseUrl}`);
+    }
+    add(pet: Pet) {
+      return this.http.post(`${this.baseUrl}/add`, pet);
+    }
+    update(pet: Pet, id: number) {
+      return this.http.put(`${this.baseUrl}/${id}`, pet);
+    }
+    delete(id: any) {
+      return this.http.delete(`${this.baseUrl}/${id}`);
+    }
+    getById(id: any) {
+      return this.http.get(`${this.baseUrl}/${id}`);
+    }
+}

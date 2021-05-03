fb80494f6472914f793e992d99a948ac4ce8eda9
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-03 14:14:35 +0300

    feat: create veterinarian component.

--- src/app/modules/veterinarians/veterinarians.component.ts
+++ src/app/modules/veterinarians/veterinarians.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Specialty } from 'src/app/model/specialty.model';
+import { Veterinarian } from 'src/app/model/veterinarian.model';
+import { SpecialtyService } from 'src/app/services/specialty.service';
+import { VeterinarianService } from 'src/app/services/veterinarian.service';
 
 @Component({
   selector: 'app-veterinarians',
@@ -7,9 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeterinariansComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'surname', 'firstname', 'patronymic','specialty','actions'];
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  
+  veterinarians: any;
+  specialties: any;
+  public dataSource: any;
+  veterinarian: Veterinarian;
+  specialty: Specialty;
+  vetForm: FormGroup;
+
+
+  constructor(private specialtyService: SpecialtyService, private veterinarianService: VeterinarianService, 
+              private formBuilder: FormBuilder, public dialog: MatDialog) { 
+
+        this.vetForm = this.formBuilder.group({
+            surname: ['', [Validators.required]],
+            firstname: ['', [Validators.required]],
+            patronymic: [''],
+            specialty: ['',[Validators.required]]
+
+        });
+
+  }
+  public get f() {
+    return this.vetForm.controls;
+  }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
+
+  create() {
+    this.veterinarianService.add(this.veterinarian).subscribe(data => {
+        this.vetForm.setErrors(null);
+        this.vetForm.reset();
+        //TODO load table data 
+    });
+
+  }
+  getSpecialties() {
+    this.specialtyService.getSpecialties().subscribe(data => {
+      this.specialties = data;
+    });
+
+  }
+  getVeterinarians() {
+    this.veterinarianService.getVeterinarians().subscribe(data => {
+      this.veterinarians = data;
+    });
+
+  }
+
+  update() {
+    this.veterinarianService.update(this.veterinarian, this.veterinarian.id);//MAYBE ERROR 
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  delete(id: number) {
+    this.veterinarianService.delete(id).subscribe(() => {
+      this.getVeterinarians();
+    });
+    
+  }
+
+
 
 }

6959b356e4cbddffb6b7d92a9a10a35f3e042ee6
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-04 19:16:08 +0300

    feat: create component for working with veterinarins info.

--- src/app/modules/veterinarians/add-modal/add-modal.component.ts
+++ src/app/modules/veterinarians/add-modal/add-modal.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Specialty } from 'src/app/model/specialty.model';
+import { Veterinarian } from 'src/app/model/veterinarian.model';
+import { SpecialtyService } from 'src/app/services/specialty.service';
+import { VeterinarianService } from 'src/app/services/veterinarian.service';
 
 @Component({
   selector: 'app-add-modal',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddModalComponent implements OnInit {
 
-  constructor() { }
+  vetForm: FormGroup;
+  dataSource: any;
+  veterinarians: any;
+  specialties: any;
+
+  constructor(public dialogRef: MatDialogRef<AddModalComponent>,
+              @Inject(MAT_DIALOG_DATA) public vet: Veterinarian, @Inject(MAT_DIALOG_DATA) public specialty: Specialty,
+              public vetService: VeterinarianService, public specialtyService: SpecialtyService, private formBuilder: FormBuilder ) {
+          
+                this.vetForm = this.formBuilder.group({
+                  surname: ['', [Validators.required]],
+                  firstname: ['', [Validators.required]],
+                  patronymic: [''],
+                  specialty: ['',[Validators.required]]
+      
+              });
+      }
 
   ngOnInit() {
+    this.getSpecialties();
+  }
+
+  load() {
+    this.vetService.getVeterinarians().subscribe((data: {}) => {
+      this.dataSource.data = data;
+      this.veterinarians = data;
+
+    })
+  }
+
+  public get f() {
+    return this.vetForm.controls;
+  }
+  
+  getSpecialties() {
+    return this.specialtyService.getSpecialties().subscribe(data =>{
+      this.specialties = data;
+    })
+  }
+
+  create() {
+    this.vetService.add(this.vetForm.value).subscribe(() => {
+
+    });
+
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }

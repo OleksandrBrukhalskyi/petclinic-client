ea2ec09734b6f28bfaba0fd6e8c1a683ff80cb51
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-04 19:55:36 +0300

    feat: implement logic in adding specialty modal component.

--- src/app/modules/specialties/add-specialty-modal/add-specialty-modal.component.ts
+++ src/app/modules/specialties/add-specialty-modal/add-specialty-modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Specialty } from 'src/app/model/specialty.model';
+import { SpecialtyService } from 'src/app/services/specialty.service';
 
 @Component({
   selector: 'app-add-specialty-modal',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-specialty-modal.component.scss']
 })
 export class AddSpecialtyModalComponent implements OnInit {
+  dataSource: any;
+  specialties: any;
+  specialtyForm: FormGroup;
+  
+  constructor(private specialtyService: SpecialtyService,private formBuilder: FormBuilder,public dialogRef: MatDialogRef<AddSpecialtyModalComponent>,
+              @Inject(MAT_DIALOG_DATA) public specialty: Specialty) { 
+    this.specialtyForm = this.formBuilder.group({
+      name: ['',[Validators.required]]
+    });
 
-  constructor() { }
+  }
+
+  create() {
+    this.specialtyService.add(this.specialtyForm.value).subscribe(() => {
+
+    });
+
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public get f() {
+    return this.specialtyForm.controls;
+  }
 
   ngOnInit() {
+    this.getSpecialties();
+  }
+  getSpecialties() {
+    this.specialtyService.getSpecialties().subscribe(data => {
+      this.dataSource.data = data;
+      this.specialties = data;
+    })
   }
 
 }

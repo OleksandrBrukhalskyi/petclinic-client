09b96cdbc7ffce17bb8c0f02536c4ffc66f20b2c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-04 19:46:21 +0300

    feat: implement logic of specialty component.

--- src/app/modules/specialties/specialties.component.ts
+++ src/app/modules/specialties/specialties.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Specialty } from 'src/app/model/specialty.model';
+import { SpecialtyService } from 'src/app/services/specialty.service';
+import { AddSpecialtyModalComponent } from './add-specialty-modal/add-specialty-modal.component';
+import { UpdateSpecialtyModalComponent } from './update-specialty-modal/update-specialty-modal.component';
 
 @Component({
   selector: 'app-specialties',
@@ -7,9 +13,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialtiesComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id','name'];
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  dataSource: any;
+  specialties: any;
+  specialty: Specialty;
+  specialtyForm: FormGroup;
+
+
+  constructor(private specialtyService: SpecialtyService,private formBuilder: FormBuilder,public dialog: MatDialog) { 
+    this.specialtyForm = this.formBuilder.group({
+      name: ['',[Validators.required]]
+    });
+
+  }
+
+  public get f() {
+    return this.specialtyForm.controls;
+  }
+
+
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource = new MatTableDataSource();
   }
+
+  create() {
+    this.specialtyService.add(this.specialty).subscribe(data => {
+      this.specialtyForm.setErrors(null);
+      this.specialtyForm.reset();
+
+    });
+
+  }
+
+  update() {
+    this.specialtyService.update(this.specialty, this.specialty.id).subscribe(data => {
+
+    });
+
+  }
+  getSpecialties() {
+    this.specialtyService.getSpecialties().subscribe(data => {
+      this.dataSource.data = data;
+      this.specialties = data;
+    })
+  }
+
+  delete(id: any) {
+    this.specialtyService.delete(id).subscribe(() => {
+      this.getSpecialties();
+    });
+
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openDialogOnCreate(){
+    const dialogRef = this.dialog.open(AddSpecialtyModalComponent,{
+      width: '800px',
+      data: {}
+
+    
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.specialty= result;
+      console.log(result)
+      this.getSpecialties();
+
+    })
+
+
+  }
+  openDialogOnUpdate(specialty: Specialty) {
+    const dialogRef = this.dialog.open(UpdateSpecialtyModalComponent, {
+        width: '800px',
+        data: specialty
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result)
+
+    })
+
+  }
+
+
 
 }

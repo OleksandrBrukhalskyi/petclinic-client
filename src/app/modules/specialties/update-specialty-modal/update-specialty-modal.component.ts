1c4842a3a4bdf14bd995100267451572c16e7d0f
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-04 19:21:39 +0300

    feat: create modal component for updating specialty in modal window.

---
+++ src/app/modules/specialties/update-specialty-modal/update-specialty-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-specialty-modal',
+  templateUrl: './update-specialty-modal.component.html',
+  styleUrls: ['./update-specialty-modal.component.scss']
+})
+export class UpdateSpecialtyModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

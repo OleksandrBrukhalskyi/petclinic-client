b70b70a0872c18529c86a18a7f5476704a7d458e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-05 22:34:30 +0300

    feat: create visit entity.

---
+++ src/app/model/visit.model.ts
@@ -0,0 +1,13 @@
+﻿import { Pet } from "./pet.model";
+
+export class Visit {
+    id: number;
+    goalOfVisit: string;
+    pet: any;
+    price: any;
+    visitDate: Date
+    
+    constructor() {
+        this.pet = new Pet();
+    }
+}

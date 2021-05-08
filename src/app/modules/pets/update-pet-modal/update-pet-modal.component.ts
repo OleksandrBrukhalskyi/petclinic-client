62c040f8a08fe9fffe8b13b7d4a5dde2712992d1
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-08 15:16:34 +0300

    feat: add snackbar method for showing message after updating pet.

--- src/app/modules/pets/update-pet-modal/update-pet-modal.component.ts
+++ src/app/modules/pets/update-pet-modal/update-pet-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MAT_DIALOG_DATA } from '@angular/material';
 import { Owner } from 'src/app/model/owner.model';
 import { Pet } from 'src/app/model/pet.model';
 import { OwnerService } from 'src/app/services/owner.service';
@@ -18,12 +18,14 @@ export class UpdatePetModalComponent implements OnInit {
   pets: any;
   owners: any;
   selectedOwner: any;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
 
 
    constructor(public dialogRef: MatDialogRef<UpdatePetModalComponent>, 
     @Inject(MAT_DIALOG_DATA) public pet: Pet, @Inject(MAT_DIALOG_DATA) public owner: Owner, public petService: PetService, public ownerService: OwnerService ,
-    private formBuilder: FormBuilder) { 
+    private formBuilder: FormBuilder,private snackBar: MatSnackBar) { 
       this.petForm = this.formBuilder.group({
         name: ['', [Validators.required]],
         breed: ['',[Validators.required]],
@@ -34,15 +36,26 @@ export class UpdatePetModalComponent implements OnInit {
 
   ngOnInit() {
     this.getOwners();
+    
+
   }
   update() {
-    this.petService.update(this.pet,this.pet.id).subscribe(res => {
-        console.log(res);
+    this.petService.update(this.pet,this.pet.id).subscribe(() => {
+        this.openSnackBarAfterPetUpdate();
     });
 
   }
   onNoClick(): void {
     this.dialogRef.close();
+    
+
+  }
+  load() {
+    this.petService.getPets().subscribe((data: {}) => {
+      this.dataSource.data = data;
+      this.pets = data;
+
+    })
   }
   getOwners() {
     this.ownerService.getOwners().subscribe((data: {}) => {
@@ -56,6 +69,14 @@ export class UpdatePetModalComponent implements OnInit {
 selectOwner(event: Event) {
   this.selectedOwner = (event.target as HTMLSelectElement).value;
   console.log(this.selectedOwner)
+}
+openSnackBarAfterPetUpdate(){
+  this.snackBar.open('The pet was successfully updated!', 'Ok',{
+    duration: 5000,
+    horizontalPosition: this.horizontalPosition,
+    verticalPosition: this.verticalPosition,
+  });
+
 }
 
 }

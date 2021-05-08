efb8d806b5edecffef2748b312b166ece70ce35d
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-08 16:38:19 +0300

    feat: create modal component for deleting veterinarians.

---
+++ src/app/modules/veterinarians/delete-modal-vet/delete-modal-vet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-modal-vet',
+  templateUrl: './delete-modal-vet.component.html',
+  styleUrls: ['./delete-modal-vet.component.scss']
+})
+export class DeleteModalVetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

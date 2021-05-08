93e7fdebff128a3314783088b9acfcb368730b7a
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-08 13:53:53 +0300

    feat: add snackbar in delete component.

--- src/app/modules/owners/modal-owner-delete/modal-owner-delete.component.ts
+++ src/app/modules/owners/modal-owner-delete/modal-owner-delete.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, InjectionToken, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MAT_DIALOG_DATA } from '@angular/material';
 import { Owner } from 'src/app/model/owner.model';
 import { OwnerService } from 'src/app/services/owner.service';
 
@@ -12,15 +12,26 @@ export class ModalOwnerDeleteComponent implements OnInit {
 
   dataSource: any;
   owners: any;
-  constructor(public dialogRef: MatDialogRef<ModalOwnerDeleteComponent>, @Inject(MAT_DIALOG_DATA) public owner: Owner, public ownerService: OwnerService) { }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  
+  constructor(public dialogRef: MatDialogRef<ModalOwnerDeleteComponent>, @Inject(MAT_DIALOG_DATA) public owner: Owner, public ownerService: OwnerService,private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
 
   delete(id:any) {
     this.ownerService.delete(id).subscribe(() => {
-      
+      this.openSnackBarAfterOwnerDelete();
     });
   } 
+  openSnackBarAfterOwnerDelete(){
+    this._snackBar.open('The owner was successfully deleted!', 'Ok',{
+      duration: 5000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+
+  }
   
 }

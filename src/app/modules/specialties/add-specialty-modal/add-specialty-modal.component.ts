4699b4fa0bc8cab9f6fa91152b39511a3787d5ab
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-08 15:30:09 +0300

    feat: add snackbar for showing message when add specialty.

--- src/app/modules/specialties/add-specialty-modal/add-specialty-modal.component.ts
+++ src/app/modules/specialties/add-specialty-modal/add-specialty-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MAT_DIALOG_DATA } from '@angular/material';
 import { Specialty } from 'src/app/model/specialty.model';
 import { SpecialtyService } from 'src/app/services/specialty.service';
 
@@ -13,9 +13,11 @@ export class AddSpecialtyModalComponent implements OnInit {
   public dataSource: any;
   specialties: any;
   specialtyForm: FormGroup;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
   
   constructor(private specialtyService: SpecialtyService,private formBuilder: FormBuilder,public dialogRef: MatDialogRef<AddSpecialtyModalComponent>,
-              @Inject(MAT_DIALOG_DATA) public specialty: Specialty) { 
+              @Inject(MAT_DIALOG_DATA) public specialty: Specialty, private snackBar: MatSnackBar) { 
     this.specialtyForm = this.formBuilder.group({
       name: ['',[Validators.required]]
     });
@@ -24,7 +26,7 @@ export class AddSpecialtyModalComponent implements OnInit {
 
   create() {
     this.specialtyService.add(this.specialtyForm.value).subscribe(() => {
-        console.log(this.specialtyForm.value)
+        this.openSnackBarAfterSpecialtyAdd();
     });
 
   }
@@ -44,6 +46,14 @@ export class AddSpecialtyModalComponent implements OnInit {
       this.dataSource.data = data;
       this.specialties = data;
     })
+  }
+  openSnackBarAfterSpecialtyAdd(){
+    this.snackBar.open('The specialty was successfully added!', 'Ok',{
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+
   }
 
 }

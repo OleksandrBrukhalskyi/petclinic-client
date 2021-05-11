6a951e6eed04295494e0b5442ebba36f56e90c9c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-05-11 20:20:09 +0300

    feat: add busines logic method into owner service.

--- src/app/services/owner.service.ts
+++ src/app/services/owner.service.ts
@@ -1,37 +1,40 @@
 ﻿import { Injectable } from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Owner } from '../model/owner.model';
 @Injectable({
-    providedIn: 'root'
-  })
-  export class OwnerService {
-  
-  
-      baseUrl = `http://localhost:8080/api/owner`;
-  
-    constructor(private http: HttpClient) { }
-  
-    getOwners() {
-        return this.http.get(`${this.baseUrl}`);
-    }
-  
-    add(owner: Owner) {
-        return this.http.post(`${this.baseUrl}/add`, owner);
-    }
-  
-    update(owner: Owner, id: number) {
-        return this.http.put(`${this.baseUrl}/${id}`, owner);
-    }
-    delete(id: any) {
-        return this.http.delete(`${this.baseUrl}/${id}`);
-    }
-  
-    getById(id: any) {
-      return this.http.get(`${this.baseUrl}/${id}`);
-    }
-    getQuantity() { 
-      return this.http.get(`${this.baseUrl}/quantity`);
-               
-    }
+  providedIn: 'root'
+})
+export class OwnerService {
+
+
+  baseUrl = `http://localhost:8080/api/owner`;
+
+  constructor(private http: HttpClient) { }
+
+  getOwners() {
+    return this.http.get(`${this.baseUrl}`);
   }
+
+  add(owner: Owner) {
+    return this.http.post(`${this.baseUrl}/add`, owner);
+  }
+
+  update(owner: Owner, id: number) {
+    return this.http.put(`${this.baseUrl}/${id}`, owner);
+  }
+  delete(id: any) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  getById(id: any) {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+  getQuantity() {
+    return this.http.get(`${this.baseUrl}/quantity`);
+
+  }
+  groupOwnersByPetsCount() {
+    return this.http.get(`${this.baseUrl}/group-owners-by-count`);
+  }
+}
